b8726a337fb19e0a3515eeb8e3a877180d275a0b
Emilly Eduarda <emillyeduardadasilvamadureira@gmail.com>
2026-09-17 15:23:37 -0400

    Adiciona condicional para tarefa

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 
 $nome = "Emilly";
+$curso = "Engenharia de Software";
+$tarefa = "Estudar PHP";
 
 echo"Olá, $nome!";
+echo"<br>";
+echo"Curso: $curso";
+echo"<br>";
+
+
+if ($tarefa != "") {
+    echo "Tarefa de hoje: $tarefa";
+} else {
+    echo "Você não tem nenhuma tarefa.";
+}
 
